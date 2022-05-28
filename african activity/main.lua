2afaac0a359a25518dcd4f389b5e05fb329a010d
fwshanks <106421440+fwshanks@users.noreply.github.com>
2022-05-28 00:48:09 -0700

    Update main.lua

--- african activity/main.lua
+++ african activity/main.lua
@@ -13,3 +13,36 @@ end
 function love.draw()
     love.graphics.draw(player.sprite, player.x, player.y)
 end
+
+function love.load()
+    player = {}
+    player.x = 400
+    player.y = 200
+end
+
+function love.update(dt)
+  if love.keyboard.isDown("right") then
+         player.x = player.x + 4
+  end
+end
+
+if love.keyboard.isDown("left") then
+    player.x = player.x - 4
+end
+end
+
+if love.keyboard.isDown("down") then
+    player.y = player.y + 4
+end
+end
+
+if love.keyboard.isDown("up") then
+    player.x = player.x - 4
+end
+end
+
+
+
+function love.draw
+ love.graphics.circle("fill", player.x, player.y, 32)
+end
